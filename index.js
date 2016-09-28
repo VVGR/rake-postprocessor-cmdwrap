c15e786f6b3f8816de8d351dcb06410943ca8614
xiewei <xiewei@zhubajie.com>
2016-09-28 20:46:16 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(content, file, conf){
     if ( file.isMod ) {
-        content = content.replace(/^(\s*define\(.*?,)(.*)(function\([\w\W]+)$/img, "$1 " + outputDep( file.requires ) + ", $3" )
+        content = content.replace(/^(\s*define\(.*?,)(.*?)(function\([\w\W]+)$/img, "$1 " + outputDep( file.requires ) + ", $3" )
     }
     return content;
 };
